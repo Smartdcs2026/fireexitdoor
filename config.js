5d08aac6a1b6c9cbf30fc1e69206688f2f1bcb3b
SKDC_SMART <somchaibutphon@gmail.com>
2026-06-09 03:39:46 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+/************************************************************
+ * config.js
+ * ระบบตรวจประตูหนีไฟประจำวัน
+ ************************************************************/
+
+window.APP_CONFIG = {
+  APP_NAME: 'ระบบตรวจประตูหนีไฟประจำวัน',
+
+  API_BASE: 'https://fireexitdoor.somchaibutphon.workers.dev',
+
+  LOGO_URL: 'https://lh5.googleusercontent.com/d/1HicYHV18UaA5y4GFyHJaG9aNI-qjIzIY',
+
+  TIMEZONE: 'Asia/Bangkok',
+
+  ROUTES: {
+    HOME: 'index.html',
+    CHECK: 'check.html',
+    VIEW: 'view.html',
+    REPORT: 'report.html',
+    QRCODE: 'qrcode.html'
+  }
+};
